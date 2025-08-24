c6bc73b5be84eec74789dc7491165fc6e451a48a
Zigyasa Ritwajeet Jha <zigyasa.r.jha@gmail.com>
2025-08-25 02:03:57 +0530

    primeNumbersInRange tested

--- strings.py
+++ strings.py
@@ -16,13 +16,32 @@ def get_sum_of_digits(n):
         last_digit = n%10 # find out last digit of number
         sum+=last_digit # add last digit to sum
         n = n//10 # truncate the last digit since it's already added
-    return sum
+    return sum 
 
 
-n = int(input("enter any number: "))
-ans = get_sum_of_digits(n)
-print(ans) 
+def get_prime_numbers(lower_limit, upper_limit):
+
+    for i in range(lower_limit, upper_limit+1): 
+        isPrime = 1 
+        for j in range(2, int(i**0.5+1)): 
+            if i%j == 0: 
+                # i is composite 
+                isPrime = 0 
+                #print(f"{j} divides {i}, let's go to next i ")
+            continue  
+        if isPrime: 
+            print(f"{i} is prime") 
+            
+
+n1 = 2
+n2 = 50
+get_prime_numbers(n1, n2)
+# n = int(input("enter any number: "))
+# ans = get_sum_of_digits(n)
+# print(ans) 
 
 
 
 
+
+# %%
