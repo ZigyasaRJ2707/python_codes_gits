46878c78157a7a5de925c3b450a4dfb4fd43eddc
Zigyasa Ritwajeet Jha <zigyasa.r.jha@gmail.com>
2025-08-25 00:24:26 +0530

    palindrome done

--- strings.py
+++ strings.py
@@ -1,8 +1,16 @@
 # %%
-def if_palindrome(n): 
+def if_palindrome(n):
+    s = str(n)
+
+    l = len(s)
+
+    for i in range(0, l//2): 
+        if s[i] != s[l-i-1]:
+            return 0
+
     return 1 
 
-n = 25 
+n = 727 
 ans = if_palindrome(n)
 print(ans)
 # %%
