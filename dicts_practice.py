ccebca30cdbac904ce66ea8c42888f35854f875c
ZigyasaRJ2707 <zigyasa.r.jha@gmail.com>
2025-12-18 16:59:36 +0530

    m

--- dicts_practice.py
+++ dicts_practice.py
@@ -1,1 +1,12 @@
-print("hello")
+# q14
+d={'spring':'autumn', 'autumn':'fall', 'fall':'spring'}
+print(d['autumn'])
+
+# q23
+d1 = {"a":10, "b":2, "c":3}
+str1=""
+for i in d1:
+    str1=str1+str(d1[i])+" "
+    str2=str1[:-1]
+    
+print(str2[::-1])    
