96a6bc76c90ce3b982275d540f80c89fa3adf74f
ZigyasaRJ2707 <zigyasa.r.jha@gmail.com>
2026-03-20 21:36:05 +0530

    m

--- tuples_dicts_edge.py
+++ tuples_dicts_edge.py
@@ -2,3 +2,8 @@ t = (1, [2, 3])
 t[1].append(4) # we can modify a LIST INSIDE a tuple, because LISTS are mutable. nice edge case
 print(t) 
 
+''' d = {}
+d[([1, 2], 3)] = "hello"   (this code gives an error, because dicts can only have hashable keys. Lists are MUTABLE
+but dict keys must be IMMUTABLE to be HASHABLE) ''' 
+
+
