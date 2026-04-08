c42c2eebe2b883c808c7cd5c5871f80b126086f2
ZigyasaRJ2707 <zigyasa.r.jha@gmail.com>
2026-04-09 00:21:18 +0530

    m

---
+++ primes.py
@@ -0,0 +1,11 @@
+def prime(n): 
+    if n < 2: 
+        return False 
+    for i in range(2, int(n**0.5)+1): 
+        if n%i == 0: 
+            return False 
+    return True 
+
+for x in range(1, 500): 
+    if prime(x): 
+        print(x)
