5d17209b3f3a8c268214c6f1489130989635041f
ZigyasaRJ2707 <zigyasa.r.jha@gmail.com>
2026-04-09 00:28:12 +0530

    works yay

---
+++ total_primes.py
@@ -0,0 +1,14 @@
+def is_prime(n): 
+    if n<2: 
+        return False 
+    for i in range(2, int(n**0.5) + 1): 
+        if n % i == 0: 
+            return False 
+    else: 
+        return True 
+total = 0 
+for k in range(1, 1000): 
+    if is_prime(k):
+         total+=1 
+
+print(total)
